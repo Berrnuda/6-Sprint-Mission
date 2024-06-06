8e812ecc2e65dd294acd98217cca30515b160db8
김제완 <gimjewan@CarpeDiem-MacBookAir.local>
2024-06-07 00:03:28 +0900

    feat: boards 페이지 페이지네이션, 검색기능 등 추가 및 수정

---
+++ types.ts
@@ -0,0 +1,80 @@
+export interface ArticlesList {
+    id: number;
+    title: string;
+    content: string;
+    image: null | string;
+    likeCount: number;
+    createdAt: string;
+    updatedAt: string;
+    writer: Writer;
+  }
+  
+  export interface Writer {
+    id: number;
+    nickname: string;
+  }
+  
+  export interface BoardsProps {
+    PostsData: ArticlesList[];
+  }
+  
+  export interface DropdownProps {
+    onChange: (order: "recent" | "like") => void;
+  }
+  
+  export interface PostsProps {
+    posts: ArticlesList;
+  }
+  
+  export interface SearchInputProps {
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  }
+
+  export interface getUserMessage {
+    message: string;
+  }
+  
+  export interface Article {
+    id: number;
+    title: string;
+    content: string;
+    image: string | null;
+    likeCount: number;
+    createdAt: string;
+    updatedAt: string;
+    writer: ArticleWriter;
+    isLiked: boolean;
+  }
+  
+  export interface ArticleWriter {
+    id: number;
+    nickname: string;
+  }
+  
+  export interface getUserData {
+    id: number;
+    nickname: string;
+    image: null | string;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  export interface Comments {
+    id: number;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    writer: CommentsWriter;
+  }
+  
+  export interface CommentsWriter {
+    id: number;
+    nickname: string;
+    image: string | null;
+  }
+  
+  export interface PostViewProps {
+    article: Article | null;
+    comments: Comments[];
+  }
